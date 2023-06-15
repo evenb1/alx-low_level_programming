ce46aaef54b057601f0fc69714304aeb7bd97243
Even <even.russom@strathmore.edu>
2023-06-15 07:54:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - prints "Programming is like buildiing a multilingual
  * puzzle, followed by a new line."
  * return 0.
  */
 
- int main(void)
+int main(void)
  
- {
- 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
